3624d4ca71f4380a458dd843f27dca2f612c0022
tenkalden <developer@tibet.net>
2024-09-17 16:43:19 +0530

    book-review

---
+++ book-review/app/Models/Book.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use DateRangeError;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Contracts\Database\query\Builder as QueryBuilder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+    public function scopeTitle(Builder $query, String $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%'. $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null ): Builder | QueryBuilder
+    {
+        return $query->withCount([
+                'reviews' => fn(Builder $q) => $this->DateRangeFilter($q, $from, $to)
+            ])
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from=null, $to=null): Builder | QueryBuilder {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->DateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder | QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null) {
+        if($from && !$to) {
+            $query->where('created_at', '>=', $from );
+        }
+        else if(!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        }else if($from && $to ) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
+}
+
---
+++ book-review/database/factories/ReviewFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'book_id' => null,
+            'review' => fake()->paragraph,
+            'rating' => fake()->numberBetween(1,5),
+            'created_at' => fake()->dateTimeBetween('-2 years'),
+            'updated_at' => function(array $attributes) {
+                return fake()->dateTimeBetween($attributes['created_at']);
+            },
+        ];
+    }
+
+    public function good()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => fake()->numberBetween(3,5)
+            ];
+        });
+    }
+
+    public function average()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'rating' => fake()->numberBetween(2,4)
+            ];
+        });
+    }
+
+    public function poor()
+    {
+        return $this->state( function (array $attributes) {
+            return [
+                'rating' => fake()->numberBetween(1,3)
+            ];
+        });
+    }
+}
